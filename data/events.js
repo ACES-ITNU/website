e2592c8ce1616bb3b94d0382f016b9490fcba7d2
zeel-codder <zeelprajapati321@gmail.com>
2022-01-08 15:32:28 +0530

    event page is done

--- data/events.js
+++ data/events.js
@@ -20,6 +20,12 @@
  *    give location is your event is offline
  * 7. by_who
  *    give the name of person our organization who's event
+ * 8. duration
+ *    give the duration of event in hours only, default is 2h
+ * 
+ * note:
+ * you can add any event of your choice, the system will find which is past and which are   
+ * upcoming events, you just need to add information in right way.
  *     
  */
 
@@ -33,12 +39,57 @@ const events = [
             `Hello there, everyone!! Prepare for the action-packed Among Us competition! Surely you've all had some excellent brainstorming and ideation sessions.`
             , `“You’ve got to stay focused without being boring - because all work and no play makes Jack a dull boy ” It's now time to have some fun and play some games. If you haven't participated in website making competition still you can participate in this. In the Discord Among Us channel, we will share the Joining Code there.`
         ],
-        'time':[2021,2,12,4,30],
+        'time':[2022,2,12,4,30],
+        'join_link':'#',
+        'is_online':false,
+        'locatiion':'n-303',
+        'by_who':'ACES',
+        'duration':2
+
+    },
+    {
+
+        'title': 'Among Us',
+        'about': [
+            `Hello there, everyone!! Prepare for the action-packed Among Us competition! Surely you've all had some excellent brainstorming and ideation sessions.`
+            , `“You’ve got to stay focused without being boring - because all work and no play makes Jack a dull boy ” It's now time to have some fun and play some games. If you haven't participated in website making competition still you can participate in this. In the Discord Among Us channel, we will share the Joining Code there.`
+        ],
+        'time':[2022,2,13,4,30],
         'join_link':'#',
         'is_online':true,
-        'by_who':'ACES'
+        'by_who':'ACES',
+        'duration':2
 
-    }
+    },
+    {
+
+        'title': 'Among Us',
+        'about': [
+            `Hello there, everyone!! Prepare for the action-packed Among Us competition! Surely you've all had some excellent brainstorming and ideation sessions.`
+            , `“You’ve got to stay focused without being boring - because all work and no play makes Jack a dull boy ” It's now time to have some fun and play some games. If you haven't participated in website making competition still you can participate in this. In the Discord Among Us channel, we will share the Joining Code there.`
+        ],
+        'time':[2021,2,13,4,40],
+        'join_link':'#',
+        'is_online':false,
+        'location':'Niram n-xyz',
+        'by_who':'ACES',
+        'duration':2
+
+    },
+    {
+
+        'title': 'Among Us',
+        'about': [
+            `Hello there, everyone!! Prepare for the action-packed Among Us competition! Surely you've all had some excellent brainstorming and ideation sessions.`
+            , `“You’ve got to stay focused without being boring - because all work and no play makes Jack a dull boy ” It's now time to have some fun and play some games. If you haven't participated in website making competition still you can participate in this. In the Discord Among Us channel, we will share the Joining Code there.`
+        ],
+        'time':[2022,0,8,15,17],
+        'join_link':'#',
+        'is_online':true,
+        'by_who':'ACES',
+        'duration':1
+
+    },
 
 
 ]
--- pages/events.js
+++ pages/events.js
@@ -3,6 +3,9 @@ import Navbar from '../components/Navbar'
 import { useState, useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowLeft, faArrowRight, faArrowCircleDown, faArrowCircleUp } from '@fortawesome/free-solid-svg-icons'
+import events from '../data/events'
+
+
 
 export default function Home(props) {
   const { active, setactive, menu, setmenu } = props
@@ -52,12 +55,12 @@ export default function Home(props) {
         }}>
           <img src={`./${images[curr]}`} />
         </div>
-        <div id='roadmapLink' onClick={() => setdown(!down)} >
+        {/* <div id='roadmapLink' onClick={() => setdown(!down)} >
           <p>Events</p>
           <FontAwesomeIcon className='icon' id="down" icon={down ? faArrowCircleUp : faArrowCircleDown} />
-        </div>
+        </div> */}
 
-        <div  style={{
+        <div style={{
           transform: down ? "translateY(-100vh)" : "translateY(0)"
         }}>
           <EventList></EventList>
@@ -68,111 +71,355 @@ export default function Home(props) {
   )
 }
 
+function GetDate(arr) {
+
+  return new Date(arr[0], arr[1], arr[2], arr[3], arr[4]);
+
+}
+
+function formatAMPM(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  return strTime;
+}
 
 function EventList() {
+
+
+  const [data, setData] = useState(events.sort((a, b) => GetDate(b.time) - GetDate(a.time)));
+  // const [data, setData] = useState(events.slice(1,2));
+  const [live, setLive] = useState([]);
+  const [past, setPast] = useState([]);
+  const [upcoming, setUpComing] = useState([]);
+  const [change, setChange] = useState(true)
+
+  useEffect(() => {
+
+    getData();
+
+  }, [])
+
+  useEffect(() => {
+
+    getData();
+
+  }, [change])
+
+
+
+  function getData() {
+
+    let p = [], l = [], u = [];
+
+    for (let i = 0; i < data.length; i++) {
+
+      if (!data[i].duration) {
+        data[i].duration = 2;
+      }
+
+      console.log(data[i])
+
+      const value = GetDate(data[i].time) - new Date();
+
+      if (value < 0) {
+        const hour = Math.floor((-value / (1000 * 60 * 60)));
+
+        console.log(hour);
+        if (hour < data[i].duration) {
+          
+          l.push(data[i]);
+          
+        } else {
+          p.push(data[i]);
+        }
+      } else {
+        
+        
+        u.push(data[i]);
+
+      }
+    }
+
+    setLive(l);
+    setPast(p);
+    setUpComing(u);
+
+  }
+
+
+
+
   return (
     <div className='Events'>
 
-      <Event></Event>
-      <Event></Event>
-      <Event></Event>
-      <Event></Event>
-      <Event></Event>
-      <Event></Event>
-      <Event></Event>
+      {
+        live.length != 0
+
+        &&
+        <>
+          <h1>Live</h1>
+          {
+
+            live.map((value, index) => {
+
+              return <Event {...value} key={index} changeList={setChange} changeValue={change}  type="live"></Event>
+
+            })
+          }
+        </>
+      }        {
+        upcoming.length != 0
+
+        &&
+        <>
+          <h1>Up Coming</h1>
+          {
+
+            upcoming.map((value, index) => {
+
+              return <Event {...value} key={index} changeList={setChange} changeValue={change}  type="upcoming"></Event>
+
+            })
+          }
+
+        </>
+
+      }{
+        past.length != 0
+
+        &&
+        <>
+          <h1>Past</h1>
+          {
+
+            past.map((value, index) => {
+
+              return <Event {...value} key={index} changeList={setChange} changeValue={change} type="past"></Event>
+
+            })
+          }
+        </>
+      }
 
     </div>
   )
 }
 
-function Event() {
+function Event({ title, about, time, join_link, is_online, by_who, duration, changeValue,changeList, type,location }) {
+
+  const date = GetDate(time);
+
+  const [DateItem, setDate] = useState(getDateValue());
+
+  console.log(DateItem)
+
+
+
+  function getDateValue(tem='') {
+
+    if(type=='past') return;
+
+    const value = GetDate(time) - new Date();
+
+
+ 
+    // if(value<0 && type=='upcoming'){
+    //   clearInterval(tem)
+    //   changeList(!changeValue)
+    //   return;
+    // }
+     if(value<0 && type=='upcoming'){
+      window.location.reload()
+      return;
+    }
+  
+
+    if(value<0){
+
+      value=-value;
+    
+    }
+
+
+    var
+      seconds = Math.floor((value / 1000) % 60),
+      minutes = Math.floor((value / (1000 * 60)) % 60),
+      hours = Math.floor((value / (1000 * 60 * 60)) % 24),
+      days = Math.floor((value) / (1000 * 60 * 60 * 24));
+
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+    days = (days < 10) ? "0" + days : days;
+
+    // if(hours>=duration){
+    //   console.log('call')
+    //   clearInterval(tem)
+    //   changeList(!changeValue);
+    //   return;
+    // }
+      if(hours>=duration && type=='live'){
+      // console.log('call')
+      // clearInterval(tem)
+      // changeList(!changeValue);
+      window.location.reload()
+      return;
+    }
+
+    let ans=[]
+    ans.push(days)
+    ans.push(hours)
+    ans.push(minutes)
+    ans.push(seconds)
+
+    return ans;
+
+
+
+  }
+
+
+  useEffect(() => {
+
+    if(type!='past'){
+
+      console.log('call me')
+
+      const tem=setInterval(() => {
+        setDate(getDateValue(tem));      
+      }, 1000);
+    }
+
+    
+
+  }
+  , [])
+
+
   return (
     <div className='item'>
 
       <div className='left_item'>
         <img src='https://images.unsplash.com/photo-1565898122623-be37b5f1e778?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80' alt='^'></img>
 
-          <h1>Among Us</h1>
+        <h1>{title}</h1>
         <div className='left-info'>
-          <div> Thu,30-01-22</div>
-          <div> 09:00 pm
+          <div> 🗓️ {date.toDateString()}</div>
+          <div>⌚ {formatAMPM(date)}
           </div>
-          <div> Online
+
+          <div>
+          📋 {is_online ? 'Online' : 'Offline'}
+          </div>
+
+          <div>
+
+          🧊 {duration} h
+
+          </div>
+
+          <div>
+
+          {is_online ? '' : `⛺ ${location}`}
+
           </div>
 
 
         </div>
+
+        {
+           type == 'upcoming'
+          &&
+
           <div className='left-time'>
             <div className='day'>
 
               <div className='time-name'>
-              days
+                days
               </div>
-              
+
               <div className='time-box'>
-                12
+                {DateItem[0]}
               </div>
-              
-              </div>
+
+            </div>
             <div className='hour'>
 
               <div className='time-name'>
-              hours
+                hours
               </div>
-              
+
               <div className='time-box'>
-                12
+              {DateItem[1]}
               </div>
-              
-              </div>
+
+            </div>
             <div className='minutes'>
 
               <div className='time-name'>
-              minutes
+                minutes
               </div>
-              
+
               <div className='time-box'>
-                12
+              {DateItem[2]}
               </div>
-              
-              </div>
+
+            </div>
             <div className='seconds'>
 
               <div className='time-name'>
-              seconds
+                seconds
               </div>
-              
+
               <div className='time-box'>
-                12
+              {DateItem[3]}
               </div>
-              
-              </div>
-            
+
+            </div>
+
           </div>
+        }
 
 
       </div>
       <div className='right-item'>
 
-        <h1>Among Us</h1>
+        <h1>{title}</h1>
         <div className='about'>
-        Hello there, everyone!! Prepare for the action-packed Among Us competition! Surely you've all had some excellent brainstorming and ideation sessions. “You’ve got to stay focused without being boring - because all work and no play makes Jack a dull boy ”
-
-It's now time to have some fun and play some games. If you haven't participated in website making competition still you can participate in this. In the Discord Among Us channel, we will share the Joining Code there.
+          {
+            about.map((p) => <div key={1}>{p}</div>)
+          }
         </div>
 
 
         <div className='right_icon'>
 
-        <div>
-          - by aces
-          
+          <div>
+            - by {by_who}
+
           </div>
 
-          <button >Join Event</button>
+          {
+            type != 'past'
+
+            &&
+            <a href={join_link} target='_blank' rel="noreferrer">
+
+
+
+              Join Event
+
+
+            </a>
+          }
 
         </div>
-        
+
       </div>
 
     </div>
