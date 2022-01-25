9adc91bbaa31ed2e4ce7948fc82fc53e1d856287
KrishaVora29 <khv2902@gmail.com>
2022-01-25 19:45:57 +0530

    addtion of social media icons in contact us page

--- pages/contact.js
+++ pages/contact.js
@@ -2,8 +2,7 @@ import Head from 'next/head'
 import Navbar from '../components/Navbar'
 import { useState, useEffect } from 'react'
 // import { data } from 'autoprefixer';
-import axios from 'axios';
-
+import axios from 'axios'
 
 export default function Home(props) {
   const { active, setactive, menu, setmenu } = props
@@ -17,55 +16,42 @@ export default function Home(props) {
     message: '',
   })
 
-  const [LoadText, setLoadText] = useState('');
+  const [LoadText, setLoadText] = useState('')
 
   const handleChange = (e) => {
     setDetails({ ...details, [e.target.name]: e.target.value })
   }
 
   const SendEmail = () => {
+    setLoadText('Loading...')
 
-    setLoadText('Loading...');
-
-    axios.post('/api/contact_mail', details)
+    axios
+      .post('/api/contact_mail', details)
       .then((data) => {
-
-        setLoadText('Send');
+        setLoadText('Send')
         setDetails({
           name: '',
           email: '',
           message: '',
         })
 
-
         setTimeout(() => {
-
-          setLoadText('');
-
+          setLoadText('')
         }, 3000)
-
-      }).catch((data) => {
-
-        setLoadText('Error');
-        setDetails({
-          name: '',
-          email: '',
-          message: '',
-        })
-
-
-        setTimeout(() => {
-
-          setLoadText('');
-
-        }, 3000)
-
       })
+      .catch((data) => {
+        setLoadText('Error')
+        setDetails({
+          name: '',
+          email: '',
+          message: '',
+        })
 
+        setTimeout(() => {
+          setLoadText('')
+        }, 3000)
+      })
   }
-
-
-
 
   return (
     <div>
@@ -137,6 +123,72 @@ export default function Home(props) {
                     Nirma University,Sarkhej - Gandhinagar Hwy, Gota, Ahmedabad,
                     Gujarat 382481
                   </p>
+                  <br></br>
+                  <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+                    <a
+                      href="https://www.facebook.com/aces.itnu/"
+                      target="_blank"
+                      class="text-rose-400"
+                    >
+                      <svg
+                        fill="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        class="w-5 h-5"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                      </svg>
+                    </a>
+
+                    <a
+                      href="https://www.instagram.com/aces_it_nu/"
+                      target="_blank"
+                      class="ml-3 text-rose-400"
+                    >
+                      <svg
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        class="w-5 h-5"
+                        viewBox="0 0 24 24"
+                      >
+                        <rect
+                          width="20"
+                          height="20"
+                          x="2"
+                          y="2"
+                          rx="5"
+                          ry="5"
+                        ></rect>
+                        <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                      </svg>
+                    </a>
+                    <a
+                      href="https://www.linkedin.com/company/association-of-computer-engineering-students/"
+                      target="_blank"
+                      class="ml-3 text-rose-400"
+                    >
+                      <svg
+                        fill="currentColor"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="0"
+                        class="w-5 h-5"
+                        viewBox="0 0 24 24"
+                      >
+                        <path
+                          stroke="none"
+                          d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+                        ></path>
+                        <circle cx="4" cy="4" r="2" stroke="none"></circle>
+                      </svg>
+                    </a>
+                  </span>
                 </div>
                 <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
                   <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">
@@ -173,7 +225,6 @@ export default function Home(props) {
                   id="name"
                   name="name"
                   className="w-full bg-slate-200 rounded border border-orange-600 focus:border-orange-600 focus:ring-2 focus:ring-orange-600 text-base outline-none text-gray-900 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-
                   onChange={(e) => handleChange(e)}
                   value={details.name}
                 />
@@ -209,18 +260,11 @@ export default function Home(props) {
                   value={details.message}
                 ></textarea>
               </div>
-              <button className="text-white bg-orange-600 border-0 py-2 px-6 focus:outline-none hover:bg-orange-600 rounded text-lg"
-
+              <button
+                className="text-white bg-orange-600 border-0 py-2 px-6 focus:outline-none hover:bg-orange-600 rounded text-lg"
                 onClick={SendEmail}
               >
-
-                {
-                  LoadText
-                    ?
-                    LoadText
-                    :
-                    'SUBMIT'
-                }
+                {LoadText ? LoadText : 'SUBMIT'}
               </button>
               <p className="text-xs text-gray-400 text-opacity-90 mt-3"></p>
             </div>
